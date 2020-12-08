868e8862ac1ba7cfac6e8e4ff540bd47dffd48b2
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2020-12-08 22:23:13 +0300

    navigation structure created

---
+++ app/navigation/AuthStack.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const AuthStack = createStackNavigator();
+import login from '../screens/auth/login/Login';
+import register from '../screens/auth/register/Register';
+
+export default AuthNavigator = () => (
+  <AuthStack.Navigator>
+    <AuthStack.Screen name="login" component={login} options={{ headerShown: false }} />
+    <AuthStack.Screen name="register" component={register} options={{ headerShown: false }} />
+  </AuthStack.Navigator>
+);
---
+++ app/screens/auth/register/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Register = () => {
+  return (
+    <View>
+      <Text>REGİSTER</Text>
+    </View>
+  );
+};
+
+export default Register;
