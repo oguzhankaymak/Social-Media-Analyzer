2759b681eac596094143c36a2ed4b7c1b186a6a4
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2020-12-10 00:05:46 +0300

    fonts types added and font load function created

--- App.js
+++ App.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import * as Updates from 'expo-updates';
+import * as Font from 'expo-font';
 
 import Navigator from './app/navigation/NavigationStack';
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    loadFonts();
+  }, []);
+
+  const loadFonts = async () => {
+    let { isAvailable } = await Updates.checkForUpdateAsync();
+    if (isAvailable) {
+      await Updates.fetchUpdateAsync();
+      await Updates.reloadAsync();
+    }
+
+    try {
+      await Font.loadAsync({
+        poppinsRegular: require('./app/assets/fonts/Poppins-Regular.ttf'),
+        poppinsSemiBold: require('./app/assets/fonts/Poppins-SemiBold.ttf'),
+        poppinsBold: require('./app/assets/fonts/Poppins-Bold.ttf'),
+      });
+    } catch (error) {
+      console.log('error on font load');
+    } finally {
+      setFontLoaded(true);
+    }
+  };
+
+  if (!fontLoaded) return null;
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Navigator />
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/theme/Fonts.js
@@ -0,0 +1,30 @@
+const size = {
+  h1: 9,
+  h11: 10,
+  h12: 11,
+  h2: 12,
+  h21: 13,
+  h22: 14,
+  h3: 15,
+  h31: 16,
+  h32: 17,
+  h4: 18,
+  h41: 19,
+  h42: 20,
+  h5: 21,
+  h51: 22,
+  h6: 23,
+  h61: 24,
+};
+
+// Font Family Types
+const type = {
+  PoppinsRegular: 'poppinsRegular',
+  PoppinsSemiBold: 'poppinsSemiBold',
+  PoppinsBold: 'poppinsBold',
+};
+
+export default {
+  type,
+  size,
+};
