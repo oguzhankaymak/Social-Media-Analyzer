895eacf87ea2854a141a1a618b1ce862d816cc8d
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2020-12-12 17:38:34 +0300

    bottom tab navigator created

--- app/navigation/AppStack.js
+++ app/navigation/AppStack.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
-export default AppNavigator = () => {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  );
-};
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/bottomTab/01_home/Home';
+import AccountScreen from '../screens/bottomTab/03_account/Account';
+
+const Tab = createBottomTabNavigator();
+
+export default AppNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="home" component={HomeScreen} />
+    <Tab.Screen name="account" component={AccountScreen} />
+  </Tab.Navigator>
+);
