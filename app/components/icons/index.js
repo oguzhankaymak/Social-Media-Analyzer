6b097c2f9564f7a0bcd5f43ddda8df12fb2551f1
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2020-12-12 23:19:33 +0300

    account screen design coded

--- app/components/icons/index.js
+++ app/components/icons/index.js
@@ -1,3 +1,5 @@
+export { default as CloseIcon } from './CloseIcon'
+export { default as EditIcon } from './EditIcon'
 export { default as HomeIcon } from './HomeIcon'
 export { default as SocialGirl } from './SocialGirl'
 export { default as UserIcon } from './UserIcon'
