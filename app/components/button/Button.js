924cdcd31a39deea03f04811548379ac9c196245
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2020-12-12 23:16:11 +0300

    button component edited for background color dynamic

--- app/components/button/Button.js
+++ app/components/button/Button.js
@@ -4,9 +4,19 @@ import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
 import styles from './styles/Styles';
 import { GlobalStyles, Colors } from '../../theme';
 
-const Button = ({ title, fetching, disabled, onPressBtn }) => {
+const Button = ({ title, fetching, disabled, onPressBtn, colorName }) => {
+  const colorCode = () => {
+    let color;
+    if (colorName === 'save') color = Colors.vividCyanLimeGreen;
+    else if (colorName === 'logout') color = Colors.vividPink;
+    else color = Colors.moderateCyan;
+    return color;
+  };
   return (
-    <TouchableOpacity disabled={disabled} style={styles.button} onPress={onPressBtn}>
+    <TouchableOpacity
+      disabled={disabled}
+      style={[styles.button, { backgroundColor: colorCode() }]}
+      onPress={onPressBtn}>
       {fetching ? (
         <View style={GlobalStyles.center}>
           <ActivityIndicator size={'small'} color={Colors.white} />
