273704d2661ddc2a50587c5cb08bd8aae006987a
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2020-12-14 00:18:51 +0300

    roundend card component created

---
+++ app/components/icons/PieChartIcon2.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+function SvgPieChartIcon2(props) {
+  return (
+    <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      width="24"
+      height="24"
+      className=""
+      {...props}>
+      <Path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
+      <Path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
+    </Svg>
+  );
+}
+
+export default SvgPieChartIcon2;
---
+++ app/components/roundCard/RoundCard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+import styles from './styles/Style';
+import { BoardListIcon, InstagramIcon, PieChartIcon, WriteIcon } from '../icons';
+import { Colors } from '../../theme';
+
+const RoundCard = ({ name, title }) => {
+  const _renderIcon = () => {
+    if (name === 'instagram') {
+      return <InstagramIcon color={textColor()} width={styles.icon.width} height={styles.icon.height} />;
+    } else if (name === 'advice') {
+      return <BoardListIcon color={textColor()} width={styles.icon.width} height={styles.icon.height} />;
+    } else if (name === 'charts') {
+      return <PieChartIcon color={textColor()} width={styles.icon.width} height={styles.icon.height} />;
+    } else if (name === 'contact') {
+      return <WriteIcon color={textColor()} width={styles.icon.width} height={styles.icon.height} />;
+    }
+  };
+
+  const cardColor = () => {
+    if (name === 'instagram') {
+      return Colors.red;
+    } else if (name === 'advice') {
+      return Colors.mediumSoftCyan;
+    } else if (name === 'charts') {
+      return Colors.softOrange;
+    } else if (name === 'contact') {
+      return Colors.moderateMagenta;
+    }
+  };
+
+  const textColor = () => {
+    return 'white';
+  };
+  return (
+    <TouchableOpacity style={[styles.card, { backgroundColor: cardColor() }]}>
+      {_renderIcon()}
+      <Text style={[styles.title, { color: textColor() }]}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default RoundCard;
