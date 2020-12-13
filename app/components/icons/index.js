2ab38cc1fa49d376a994e0d57024689655660b30
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2020-12-14 00:25:44 +0300

    index.js edited in icons

--- app/components/icons/index.js
+++ app/components/icons/index.js
@@ -3,8 +3,6 @@ export { default as CloseIcon } from './CloseIcon';
 export { default as EditIcon } from './EditIcon';
 export { default as HomeIcon } from './HomeIcon';
 export { default as InstagramIcon } from './InstagramIcon';
-export { default as LockClosedIcon } from './LockClosedIcon';
-export { default as LockOpenIcon } from './LockOpenIcon';
 export { default as PieChartIcon } from './PieChartIcon';
 export { default as SocialGirl } from './SocialGirl';
 export { default as UserIcon } from './UserIcon';
