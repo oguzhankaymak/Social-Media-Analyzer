b4b92b2ac346f471dbdd91a6cb464f95f04aed93
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2020-12-20 23:57:07 +0300

    my charts stack created

--- app/navigation/stacks/OperationStack.js
+++ app/navigation/stacks/OperationStack.js
@@ -4,10 +4,12 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 import Contact from '../../screens/operations/contact/Contact';
+import MyChartsStack from './MyChartsStack';
 
 const OperationStack = () => (
   <Stack.Navigator headerMode="none">
     <Stack.Screen name="contact" component={Contact} />
+    <Stack.Screen name="myChartsStack" component={MyChartsStack} />
   </Stack.Navigator>
 );
 
