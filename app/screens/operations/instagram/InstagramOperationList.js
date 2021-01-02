a26b15b0aa028b9abf5458ead8160e15faaea5c3
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-01-02 14:35:49 +0300

    instagram operation list screen coded

--- app/screens/operations/instagram/InstagramOperationList.js
+++ app/screens/operations/instagram/InstagramOperationList.js
@@ -1,11 +1,52 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+
+import styles from './styles/InstagramOperationStyle';
+import Layout from '../../../components/layout/Layout';
+import VerticalLinearCard from '../../../components/verticalLinearCard/VerticalLinearCard';
+import { KeyIcon, CreditCardIcon, InfoIcon } from '../../../components/icons';
+import { Colors } from '../../../theme';
 
 const InstagramOperationList = () => {
   return (
-    <View>
-      <Text>İnstagram</Text>
-    </View>
+    <Layout>
+      <View style={styles.container}>
+        <VerticalLinearCard
+          headerTitle={'Açık Hesap İşlemleri'}
+          linearColor={Colors.darkModerateBlueVeryDarkBlueLinearGradient}>
+          <View style={styles.content}>
+            <KeyIcon stroke={'white'} />
+            <Text style={styles.text}>Herhangi bir şifre bilgisi vermeden işlemlerinizi gerçekleştirebilirsiniz.</Text>
+          </View>
+          <View style={styles.content}>
+            <InfoIcon stroke={'white'} />
+            <Text style={styles.text}>Hesabınızı kısa süreliğine açık hesap haline getirip devam edebilirsiniz.</Text>
+          </View>
+          <View style={styles.content}>
+            <CreditCardIcon stroke={'white'} />
+            <Text style={styles.text}>Hiçbir ücret ödemeden günlük 5 hakkınızı kullanabilirsiniz.</Text>
+          </View>
+        </VerticalLinearCard>
+        <VerticalLinearCard
+          headerTitle={'Gizli Hesap İşlemleri'}
+          linearColor={Colors.pureRedBrightRedLinearGradientColor}>
+          <View style={styles.content}>
+            <KeyIcon stroke={'white'} />
+            <Text style={styles.text}>
+              Hesabınız gizli olduğu için maalesef içeriklere erişemiyoruz dolayısıyla şifre bilgisi gerekmektedir.
+            </Text>
+          </View>
+          <View style={styles.content}>
+            <InfoIcon stroke={'white'} />
+            <Text style={styles.text}>Şifre bilginiz hiçbir şekilde kaydedilmemektedir.</Text>
+          </View>
+          <View style={styles.content}>
+            <CreditCardIcon stroke={'white'} />
+            <Text style={styles.text}>Hiçbir ücret ödemeden günlük 5 hakkınızı kullanabilirsiniz.</Text>
+          </View>
+        </VerticalLinearCard>
+      </View>
+    </Layout>
   );
 };
 
--- app/screens/operations/instagram/styles/InstagramOperationStyle.js
+++ app/screens/operations/instagram/styles/InstagramOperationStyle.js
@@ -6,9 +6,24 @@ export default StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'space-around',
   },
 
   chartTypeCardView: {
     marginVertical: hp('2%'),
+  },
+
+  content: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+  },
+
+  text: {
+    fontFamily: Fonts.type.PoppinsRegular,
+    fontSize: Fonts.size.h21,
+    color: Colors.white,
+    marginLeft: 10,
   },
 });
