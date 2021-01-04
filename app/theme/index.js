98681db2368060f7292fab44f50770cc6ddf5df4
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-01-04 21:19:51 +0300

    instagram operation form modal component created

--- app/theme/index.js
+++ app/theme/index.js
@@ -1,5 +1,6 @@
 import Colors from './Colors';
 import Fonts from './Fonts';
 import GlobalStyles from './GlobalStyles';
+import Metrics from './Metrics';
 
-export { Colors, Fonts, GlobalStyles };
+export { Colors, Fonts, GlobalStyles, Metrics };
