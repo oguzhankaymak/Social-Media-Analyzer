b4615899355b2403b05de6862afe55dc4fde0394
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-01-07 22:30:30 +0300

    instagram operation screen created

--- app/navigation/stacks/InstagramOperationStack.js
+++ app/navigation/stacks/InstagramOperationStack.js
@@ -4,10 +4,12 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 import InstagramOperationList from '../../screens/operations/instagram/InstagramOperationList';
+import InstagramOperation from '../../screens/operations/instagram/InstagramOperation';
 
 const InstagramOperationStack = () => (
   <Stack.Navigator headerMode="none">
     <Stack.Screen name="instagramOperationList" component={InstagramOperationList} />
+    <Stack.Screen name="instagramOperation" component={InstagramOperation} />
   </Stack.Navigator>
 );
 
