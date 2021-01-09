fdebd25f750f15f6c56e0bf04f3a0fb100d6bc42
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-01-09 23:17:22 +0300

    new icons added

---
+++ app/components/icons/CommentIcon.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+function SvgCommentIcon(props) {
+  return (
+    <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      width="24"
+      height="24"
+      className=""
+      {...props}>
+      <Path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+      />
+    </Svg>
+  );
+}
+
+export default SvgCommentIcon;
