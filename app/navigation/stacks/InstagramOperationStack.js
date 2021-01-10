c2379313ad376d475ef983707fba445560f93516
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-01-10 19:34:16 +0300

    followers screen created and design coded

--- app/navigation/stacks/InstagramOperationStack.js
+++ app/navigation/stacks/InstagramOperationStack.js
@@ -5,11 +5,13 @@ import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
 import InstagramOperationList from '../../screens/operations/instagram/InstagramOperationList';
 import InstagramOperation from '../../screens/operations/instagram/InstagramOperation';
+import Followers from '../../screens/operations/instagram/Followers';
 
 const InstagramOperationStack = () => (
   <Stack.Navigator headerMode="none">
     <Stack.Screen name="instagramOperationList" component={InstagramOperationList} />
     <Stack.Screen name="instagramOperation" component={InstagramOperation} />
+    <Stack.Screen name="followers" component={Followers} />
   </Stack.Navigator>
 );
 
