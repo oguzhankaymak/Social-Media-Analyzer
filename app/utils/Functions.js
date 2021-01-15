fc14b04263079ab16751c018f6d74ccbff70d0d6
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-01-15 20:57:21 +0300

    messages file created

--- app/utils/Functions.js
+++ app/utils/Functions.js
@@ -1,15 +1,12 @@
 import { Alert } from 'react-native';
+import Messages from './Messages';
 
 const emailIsValid = (email) => {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 };
 
 const generalErrorMessage = () => {
-  return Alert.alert(
-    'Maalesef İşleminiz Gerçekleştirilemedi!',
-    'Bir sorun oluştu lütfen daha sonra tekrar deneyiniz.',
-    [{ text: 'Tamam' }],
-  );
+  return Alert.alert(Messages.generalErrorTitle, Messages.generalErrorContent, [{ text: Messages.okay }]);
 };
 
 export { emailIsValid, generalErrorMessage };
