e0e68d9fb8a87e6dbd0cb48c9c343001f974ff39
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-01-15 19:41:33 +0300

    general error message function created

--- app/utils/Functions.js
+++ app/utils/Functions.js
@@ -1,5 +1,15 @@
+import { Alert } from 'react-native';
+
 const emailIsValid = (email) => {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 };
 
-export { emailIsValid };
+const generalErrorMessage = () => {
+  return Alert.alert(
+    'Maalesef İşleminiz Gerçekleştirilemedi!',
+    'Bir sorun oluştu lütfen daha sonra tekrar deneyiniz.',
+    [{ text: 'Tamam' }],
+  );
+};
+
+export { emailIsValid, generalErrorMessage };
